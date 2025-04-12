a366ce19d423d502c6aa866e1ed2e6f756033478
Linda <linda@example.com>
2025-04-12 19:20:55 +0200

    Fix: Revertir import PrismaClient à '@prisma/client' pour corriger build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 // Déclare une variable globale pour stocker l'instance de PrismaClient
